e3e5476f28a1ba347bcef52db5d8f3f5f1310efa
Simon <S.platiotis@gmail.com>
2021-01-05 22:47:43 +0200

    Formatting changes on RMIInterface

--- src/main/java/firstAssignment/RMIInterface.java
+++ src/main/java/firstAssignment/RMIInterface.java
@@ -5,13 +5,13 @@ import java.rmi.RemoteException;
 
 public interface RMIInterface extends Remote {
 
-   /**
-   *  Sends 2 matrices and specify the required operation to the server.
-   *  @param firstMatrix int[][]
-   *  @param secondMatrix int[][]
-   *  @param operation Enum(summation,subtraction,multiplication,matrix multiplication)
-   *  @return resultMatrix int[][]
-   */
-   public int[][] serverSideOperation(int[][] firstMatrix, int[][] secondMatrix, String operation) throws RemoteException;
+	/**
+	*  Sends 2 matrices and specify the required operation to the server.
+	*  @param firstMatrix int[][]
+	*  @param secondMatrix int[][]
+	*  @param operation Enum(summation,subtraction,multiplication,matrix multiplication)
+	*  @return resultMatrix int[][]
+	*/
+	public int[][] serverSideOperation(int[][] firstMatrix, int[][] secondMatrix, String operation) throws RemoteException;
 
 }
